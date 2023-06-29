8068f08b63e7e9d2838e21ef6e6201eae8d95789
WinterLimited <chami0205@gmail.com>
2023-06-29 19:27:19 +0900

    spring-initalizr

---
+++ src/main/java/haksikbot/personalized/PersonalizedApplication.java
@@ -0,0 +1,13 @@
+package haksikbot.personalized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haksikbot/personalized/PersonalizedApplicationTests.java
@@ -0,0 +1,13 @@
+package haksikbot.personalized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
